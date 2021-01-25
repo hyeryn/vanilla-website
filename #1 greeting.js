0f67b4071fb7d2bd83d842d70cc952951f7df18e
hyeryn <57982899+hyeryn@users.noreply.github.com>
2021-01-25 22:05:19 +0900

    Update #1 greeting.js

--- #1 greeting.js
+++ #1 greeting.js
@@ -5,6 +5,23 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+  event.preventDefault(); // 프로그램 동작을 막음
+  const currentValue = input.value;
+  //console.log(currentValue);
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit",handleSubmit);
+}
+
 function paintGreeting(text){
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +31,7 @@ function paintGreeting(text){
 function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
   if(currentUser === null){
-
+      askForName();
   } else {
     paintGreeting(currentUser);
   }
